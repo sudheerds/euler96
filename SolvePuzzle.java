b8dcd6467fad90912f5201bf183eb13ed82ac02f
Sudheer Depally <sudheerds@ymail.com>
2015-09-18 16:42:09 +0530

    solved problem, takin long time

--- SolvePuzzle.java
+++ SolvePuzzle.java
@@ -5,48 +5,84 @@ import java.util.TreeSet;
 
 class SolvePuzzle {
 	
-	static void solve(int[][] g){
+	static void solve(int[][] g){	
+		
+		System.out.println("Solving...\n");
 		
 		Set<Integer> blk = new TreeSet<Integer>();
-	
+		int j=0;
+		
 		for(int row=0;row < g.length; row++){
-			for(int col=0;col < g[0].length; col++){				
+			for(int col=0;col < g[row].length; col++){		
 				
-				//row and col constraints
-				for(int i=0;i<9;i++){
+				if(g[row][col]==0){					
+				
+					//row and col constraints
+					for(int i=0;i<9;i++){
+						
+						if(g[row][i]!=0)
+							blk.add(g[i][row]);
+						
+						if(g[i][col]!=0)
+							blk.add(g[i][col]);
+				}// end of row,col constraints
+				
+					//block constraints
+					for(int r = (row/3)*3,r1=r; r<r1+3; r++){
+						for(int c = ((col%9)/3)*3,c1=c; c<c1+3; c++){						
+							if(g[r][c]!=0)
+								blk.add(g[r][c]);						
+						}
+					}//end of block constraints	
 					
-					if(g[i][0]!=0)
-						blk.add(g[i][0]);
+						//logic to add number
+					    int count=0;
+						
+						do{	
+							count++;
+							if(count>9){
+								g[row][col]=0;
+								col-=2;
+								if(col<-1){
+									row--;
+									col=8;
+								}
+								break;
+							}
+								
+							if(j+1>9)
+								j=1;
+							else
+								j++;
+							
+							if(!(blk.contains(j))){
+								g[row][col]=j;
+								count=0;
+								break;
+							}
+								
+						}while(true);
 					
-					if(g[0][i]!=0)
-						blk.add(g[0][i]);
+						
+					
+				}// else if non zero
+				
+					blk.removeAll(blk);
+					
+			}// end of col for loop
+				
+				
+		}// end of row loop
+		System.out.println("\n \n \n The solved puzzels are: \n \n");
+		for(int i=0;i<9;i++){
+			for(int k=0;k<9;k++){
+				System.out.println(g[i][k]);
 			}
-			
-				//block constraints
-				//int b = 3 *((row/3)%3) + ((col/3)%3);
-				
-				if(g[row+1][col+1]!=0)
-					blk.add(g[row+1][col+1]);
-				
-				if(g[row+1][col+2]!=0)
-					blk.add(g[row+1][col+2]);
-				
-				if(g[row+2][col+1]!=0)
-					blk.add(g[row+2][col+1]);
-				
-				if(g[row+2][col+2]!=0)
-					blk.add(g[row+1][col+1]);
-				
-		}
-			
+			System.out.println();
 		}
 		
 			
-			
-	
-		
-		
-		
 	}
 
+	
 }
